9e0a95a8a7c43941bf618a17b9229b9c7095fcce
Peter Swire <swirepe@pswire-devlaptop.dhcp.tripadvisor.com>
2013-05-22 18:30:41 -0400

    formatting

--- get.sh
+++ get.sh
@@ -3,22 +3,22 @@ COLOR_off='\033[0m'
 
 _START_TIME=$(date +%s)
 echo -e 
-echo -e "${COLOR_BPurple}[get.sh]${COLOR_off} Cloning arxiv repository."
+echo -e "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} Cloning arxiv repository."
 pushd . > /dev/null
 git clone git://github.com/swirepe/arXiv-fortunes.git ~/.arxiv
 cd ~/.arxiv
 
-echo -e  "${COLOR_BPurple}[get.sh]${COLOR_off} Now removing the things you don't need."
+echo -e  "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} Now removing the things you don't need."
 rm arxiv.tar.xz
 rm  getArxiv.py
 rm parseArxiv.py
 unxz arxiv_fortunes.txt.xz
 
-echo -e "${COLOR_BPurple}[get.sh]${COLOR_off} This is a fortune:"
+echo -e "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} This is a fortune:"
 fortune .
 
 
-echo -e "${COLOR_BPurple}[get.sh]${COLOR_off} Adding the fortune command to your shellrc"
+echo -e "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} Adding the fortune command to your shellrc"
 
 RANDOM_FORTUNE_STR='
 ## display a random paper from arxiv.org
@@ -30,22 +30,24 @@ fi'
 
 SHELL_NAME=$(basename "$SHELL")
 
-if [[ "$SHELL_NAME" == "bash" ]] 
+if [[ "$SHELL_NAME" == "bash" && -f ~/.bashrc]] 
 then
+    echo "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} Adding to ~/.bashrc"
     echo  "$RANDOM_FORTUNE_STR" >> ~/.bashrc
 elif [[ "$SHELL_NAME" == "zsh" ]]
 then
+    echo "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} Adding to ~/.zshrc"
     echo  "$RANDOM_FORTUNE_STR" >> ~/.zshrc
 else
-    echo -e "${COLOR_BPurple}[get.sh]${COLOR_off} Not sure what shell you are using..."
+    echo -e "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} Not sure what shell you are using..."
     GUESS_RC="~/.${SHELL_NAME}rc"
-    echo -e "${COLOR_BPurple}[get.sh]${COLOR_off} Putting the random fortune in $GUESS_RC"
+    echo -e "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} Adding fortune to $GUESS_RC"
     echo -e "$RANDOM_FORTUNE_STR" >> $GUESS_RC
 fi
 
 popd > /dev/null
 
-echo -e "${COLOR_BPurple}[get.sh]${COLOR_off} Done."
+echo -e "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} Done."
 _END_TIME=$(date +%s)
-echo -e "${COLOR_BPurple}[get.sh]${COLOR_off} Finished in $(echo -e "$_END_TIME - $_START_TIME" | bc -l ) seconds."
+echo -e "${COLOR_BPurple}[arxiv-fortune]${COLOR_off} Finished in $(echo -e "$_END_TIME - $_START_TIME" | bc -l ) seconds."
  
